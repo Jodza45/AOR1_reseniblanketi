2a62587cb671646c8edc86227aaaf67fd28fd1a8
Jodza45 <150351723+Jodza45@users.noreply.github.com>
2024-02-17 12:27:55 +0100

    Main.c

--- 2022_oktobar2_zad2/Main.c
+++ 2022_oktobar2_zad2/Main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
 
+
+extern int transformisi(char s[]);
+
+int main() {
+		
+	char s[100];
+	scanf("%s", s);
+	int x = transformisi(s);
+	printf("%s", s);
+
+	return 0;
+}
